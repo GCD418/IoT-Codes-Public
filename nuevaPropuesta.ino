eda9bbc766783f57524dfa94bbd5252ef380abe9
gcd418 <patosquare418@gmail.com>
2025-03-13 15:53:51 -0400

    Aun no funciona

--- nuevaPropuesta.ino
+++ nuevaPropuesta.ino
@@ -1,6 +1,7 @@
 int inches = 0;
 int cm = 0;
 const int CANT_LEDS = 3;
+UltrasonicSensor us(5, 4); //T, E
 
 
 class Led {
@@ -29,9 +30,9 @@ public:
     if (this->state == STATE_OFF) turnOff();
     else if (this->state == STATE_ON) turnOn();
     else if (this->state == STATE_BLINK) {
-      if (blinkMillis == 0) setState(STATE_OFF);
-      turnOff();
+      if (blinkMillis == 0) setState(STATE_ON);
       previousMillis = millis();
+      turnOn();
     }
   }
 
@@ -61,13 +62,11 @@ private:
 };
 
 
-class UltrasonicSensor
-{
+class UltrasonicSensor {
 private:
   int triggerPin, echoPin;
   int distancia;
-  long readUltrasonicTime()
-  {
+  long readUltrasonicTime() {
     pinMode(triggerPin, OUTPUT);  // Clear the trigger
     digitalWrite(triggerPin, LOW);
     delayMicroseconds(2);
@@ -81,54 +80,60 @@ private:
   }
 
 public:
-  UltrasonicSensor(int triggerPin, int echoPin)
-  {
+  UltrasonicSensor(int triggerPin, int echoPin) {
     this->triggerPin = triggerPin;
     this->echoPin = echoPin;
   }
 
-  long readCm()
-  {
+  long readCm() {
     return 0.01723 * readUltrasonicTime();
   }
 
-  long readInches()
-  {
-    return (0.01723 * readUltrasonicTime())/ 2.54;
+  long readInches() {
+    return (0.01723 * readUltrasonicTime()) / 2.54;
   }
 };
 
 
-Led leds[] = {Led(8), Led(9), Led(10)};
-byte tiempo_blink_leds[CANT_LEDS];
+Led leds[] = { Led(8), Led(9), Led(10) };
+byte tiempo_blink_leds;
 
-void setup()
-{
-  Serial.begin(9600);
+void setup() {
+  Serial.begin(115200);
   //leds[0].setState(Led::STATE_ON);
-  for(int i = 0; i < CANT_LEDS; i ++)
+  for (int i = 0; i < CANT_LEDS; i++)
     leds[0].setState(Led::STATE_OFF);
 }
 
-void loop()
-{
-  UltrasonicSensor us = UltrasonicSensor(2, 3);
-  // measure the ping time in cm
-
+void loop() {
   cm = us.readCm();
   // convert to inches by dividing by 2.54
   Serial.print(cm);
   Serial.println("cm");
-  tiempo_blink_leds[0] = (map(cm, 3, 15, 0, 7), 0, 7);
-  Serial.println(tiempo_blink_leds[0]);
-  tiempo_blink_leds[1] = (map(cm, 16, 35, 0, 7), 0, 7);
-  Serial.println(tiempo_blink_leds[1]);
-  tiempo_blink_leds[2] = (map(cm, 36, 50, 0, 7), 0, 7);
-  Serial.println(tiempo_blink_leds[2]);
-  for(int i = 0; i < CANT_LEDS; i ++)
-  {
-    leds[i].setBlinksPerSecond(tiempo_blink_leds[i]);
-    leds[i].touch();
+  if (cm > 30) {
+    for (int i = 0; i < CANT_LEDS; i++) {
+      leds[i].setState(Led::STATE_OFF);
+      //leds[i].touch();
+    }
   }
-  delay(100); // Wait for 100 millisecond(s)
+  else {
+    if (cm <= 3) {
+      for (int i = 0; i < CANT_LEDS; i++) {
+        leds[i].setState(Led::STATE_ON);
+        //leds[i].touch();
+      }
+    } 
+    else {
+      tiempo_blink_leds = map(cm, 4, 29, 5, 1);
+      Serial.println(tiempo_blink_leds);
+      for (int i = 0; i < CANT_LEDS; i++) {
+        leds[i].setBlinksPerSecond(tiempo_blink_leds);
+        leds[i].setState(Led::STATE_BLINK);
+      }
+    }
+  }
+
+  for(int i = 0; i < CANT_LEDS; i ++)
+    leds[i].touch();
+  delay(100);
 }
