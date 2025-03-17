5323b804234c120258fd1da75d3294f7a7a047e4
christian-ledezma <158978534+christian-ledezma@users.noreply.github.com>
2025-03-17 19:23:48 -0400

    Update projectCode.ino

--- projectCode/projectCode.ino
+++ projectCode/projectCode.ino
@@ -1,7 +1,7 @@
 #include "UltrasonicSensor.h"
 #include "LedsModule.h"
 
-int cm = 0;
+int centimeters = 0;
 UltrasonicSensor us(5, 4); // T, E
 LedsModule leds;
 byte blinkingTime;
@@ -14,23 +14,23 @@ void setup()
 
 void loop()
 {
-  cm = us.readCentimeters();
+  centimeters = us.readCentimeters();
   // convert to inches by dividing by 2.54
-  Serial.print(cm);
+  Serial.print(centimeters);
   Serial.println("cm");
-  if (cm >= 30)
+  if (centimeters >= 30)
   {
     leds.turnOff();
   }
   else
   {
-    if (cm <= 3)
+    if (centimeters <= 3)
     {
       leds.turnOn();
     }
     else
     {
-      blinkingTime = map(cm, 4, 29, 20, 1);
+      blinkingTime = map(centimeters, 4, 29, 20, 1);
       Serial.println(blinkingTime);
       leds.setBlinkPerSeconds(blinkingTime);
     }
